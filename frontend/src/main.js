2325e859f5093dca22c5ba8487d1b85827100230
Ibrahim Abdul Mujeeb <ib@Ibrahims-MacBook-Pro.local>
2022-07-04 12:37:29 +0530

    Added the update feature

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
 import vuetify from './plugins/vuetify'
 
 // import axios from 'axios'
@@ -10,7 +10,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
