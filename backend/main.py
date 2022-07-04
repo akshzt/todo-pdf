e17e8263347fe86cf914de7b24ade77ab308f107
Akshat <akyvzz@gmail.com>
2022-07-04 12:48:44 +0530

    main

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-from os import stat
 from fastapi import FastAPI, status, HTTPException, Depends
 from database import Base, engine, SessionLocal
 import models
@@ -6,37 +5,13 @@ import schemas
 from typing import List
 from sqlalchemy.orm import Session
 from tasks import create_pdf_task
-from PDFUtil import PDF
-# from celery import Celery
-import os
-# import time
-# import psycopg2
-#
-# conn = psycopg2.connect(
-#     database="postgres", user="postgres", password="mysecretpassword",
-#     host="127.0.0.1", port="5432"
-# )
-#
-# conn.autocommit = True
-# cursor = conn.cursor()
+from fastapi.responses import FileResponse
 
-# from config.celery_utils import create_celery
 
 Base.metadata.create_all(engine)
 
 
-# def create_pdf(todos):
-#     pdf = PDF()
-#     pdf.add_page()
-#     for i in range(0, len(todos)):
-#         pdf.cell(0, 10, f"ID: {todos[i].id}  TASK: {todos[i].task}", new_x="LMARGIN", new_y="NEXT")
-#     pdf.output("data_pdf.pdf")
-
-
 app = FastAPI()
-# celery = Celery(__name__)
-# celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
-# celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "rpc://")
 
 
 def get_session():
@@ -52,6 +27,11 @@ def root():
     return "todooo"
 
 
+@app.get("/todo/file")
+def get_pdf_file():
+    return FileResponse(path="static/data_pdf.pdf")
+
+
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session)):
     tododb = models.ToDo(task=todo.task)
@@ -60,6 +40,7 @@ def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session
     session.commit()
     session.refresh(tododb)
 
+    create_pdf_task.delay()
     return tododb
 
 
@@ -95,7 +76,7 @@ def delete_todo(id: int, session: Session = Depends(get_session)):
         session.commit()
     else:
         raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
-
+    create_pdf_task.delay()
     return None
 
 
@@ -104,16 +85,3 @@ def read_todo_list(session: Session = Depends(get_session)):
     todo_list = session.query(models.ToDo).all()
 
     return todo_list
-
-
-# @celery.task(name='create_pdf_task')
-# def create_pdf_task(seconds):
-#     print("in create")
-#     cursor.execute('SELECT * FROM todos')
-#     result = cursor.fetchall()
-#     print(result)
-#     conn.commit()
-#     conn.close()
-#     time.sleep(seconds)
-#     create_pdf(result)
-#     return True
